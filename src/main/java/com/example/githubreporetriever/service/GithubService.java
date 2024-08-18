445503c87f3e3fdf562f9aafd241f2656091ec01
Jan <janekrembikowski@gmail.com>
2024-08-18 14:54:27 +0200

    add asynchronous branches fetching to improve performance and github api token authorization

--- src/main/java/com/example/githubreporetriever/service/GithubService.java
+++ src/main/java/com/example/githubreporetriever/service/GithubService.java
@@ -1,53 +1,62 @@
-        package com.example.githubreporetriever.service;
+package com.example.githubreporetriever.service;
 
-        import com.example.githubreporetriever.model.Branch;
-        import com.example.githubreporetriever.model.Repository;
-        import com.example.githubreporetriever.model.dto.BranchDto;
-        import com.example.githubreporetriever.model.dto.RepositoryDto;
-        import org.springframework.beans.factory.annotation.Value;
-        import org.springframework.boot.web.client.RestTemplateBuilder;
-        import org.springframework.http.HttpEntity;
-        import org.springframework.http.HttpHeaders;
-        import org.springframework.http.HttpMethod;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.stereotype.Service;
-        import org.springframework.web.client.RestTemplate;
+import com.example.githubreporetriever.model.Branch;
+import com.example.githubreporetriever.model.Repository;
+import com.example.githubreporetriever.model.dto.BranchDto;
+import com.example.githubreporetriever.model.dto.RepositoryDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-        import java.util.Arrays;
-        import java.util.List;
-        import java.util.Objects;
-        import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-        @Service
-        public class GithubService {
+import java.util.concurrent.CompletableFuture;
 
-            private final RestTemplate restTemplate;
-            private final String githubToken;
+@Service
+public class GithubService {
 
-            public GithubService(RestTemplateBuilder restTemplateBuilder, @Value("${github.api.token}") String githubToken) {
-                this.restTemplate = restTemplateBuilder.build();
-                this.githubToken = githubToken;
-            }
+    private final RestTemplate restTemplate;
+    private final String githubToken;
 
-            public List<RepositoryDto> getRepositories(String username) {
-                    String url = "https://api.github.com/users/" + username + "/repos";
-                HttpHeaders headers = new HttpHeaders();
-                headers.set("Authorization", "Bearer " + githubToken);
+    public GithubService(RestTemplateBuilder restTemplateBuilder, @Value("${github.api.token}") String githubToken) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.githubToken = githubToken;
+    }
 
-                HttpEntity<String> entity = new HttpEntity<>(headers);
-                ResponseEntity<Repository[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, Repository[].class);
-                return Arrays.stream(Objects.requireNonNull(response.getBody()))
-                        .filter(repo -> !repo.isFork())
-                        .map(repo -> new RepositoryDto(repo.getName(), repo.getOwner().getLogin(), getBranches(repo)))
-                        .collect(Collectors.toList());
-            }
+    public List<RepositoryDto> getRepositories(String username) {
+        String url = "https://api.github.com/users/" + username + "/repos";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + githubToken);
 
-            private List<BranchDto> getBranches(Repository repo) {
-                String url = "https://api.github.com/repos/" + repo.getOwner().getLogin() + "/" + repo.getName() + "/branches";
-                ResponseEntity<Branch[]> response = restTemplate.getForEntity(url, Branch[].class);
-                return Arrays.stream(Objects.requireNonNull(response.getBody()))
-                        .map(branch -> new BranchDto(branch.getName(), branch.getCommit().getSha()))
-                        .collect(Collectors.toList());
-            }
-        }
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<Repository[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, Repository[].class);
 
+        return Arrays.stream(Objects.requireNonNull(response.getBody()))
+                .filter(repo -> !repo.isFork())
+                .map(repo -> new RepositoryDto(repo.getName(), repo.getOwner().getLogin(), getBranchesAsync(repo).join()))
+                .collect(Collectors.toList());
+    }
+
+    private CompletableFuture<List<BranchDto>> getBranchesAsync(Repository repo) {
+        return CompletableFuture.supplyAsync(() -> {
+            String url = "https://api.github.com/repos/" + repo.getOwner().getLogin() + "/" + repo.getName() + "/branches";
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + githubToken);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            ResponseEntity<Branch[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, Branch[].class);
+
+            return Arrays.stream(Objects.requireNonNull(response.getBody()))
+                    .map(branch -> new BranchDto(branch.getName(), branch.getCommit().getSha()))
+                    .collect(Collectors.toList());
+        });
+    }
+}
